9f0202105b37b7c08467b03f886ba208c9a99626
shirajira <contact@novel-stud.io>
2021-05-21 23:36:12 +0900

    デバッグ環境用に Bundle ID と AppIcon を設定。

--- family-account/AppDelegate.swift
+++ family-account/AppDelegate.swift
@@ -25,6 +25,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
+        // Wait for splash for 500ms.
         presentSplash(for: 500)
 
         return true
